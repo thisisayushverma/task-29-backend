52ba686fda08f7afd6645336ef55cc9772030e9f
Ayush Verma <devilofilluminati@gmail.com>
2025-04-17 23:08:23 +0530

    complete backend

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import fs from "node:fs/promises"
+import { createReadStream } from "node:fs";
+import cors from "cors"
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+
+const reqLogger = (req,res,next)=>{
+    console.log("req:-",req.url);
+    next();
+}
+
+app.get("/",async (req,res)=>{
+    await fs.readdir("./public")
+    .then((files)=>{
+        res.status(200).json({
+            message:"success",
+            data:files.filter((file)=>file !== "favicon.ico")
+        });
+    })
+    .catch((err)=>{
+        res.status(501).json({
+            message:"server error",
+            error:err
+        })
+    })
+
+})
+
+app.get('/:id',reqLogger,async (req,res)=>{
+    const id = req.params.id;
+    const readStream = createReadStream(`./public/${id}`);
+    res.writeHead(200,{'Content-Type':'video/mp4'});
+    readStream.pipe(res);
+    // console.log(readStream.pipe());
+})
+
+
+
+
+
+app.listen(4000,()=>{
+    console.log("server is running on port 4000");
+})
